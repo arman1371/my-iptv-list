330eaaeef07b9d4acf21a855704a2b7d01a995d1
arman1371 <arman.yazdani.10@gmail.com>
2024-04-12 19:29:28 +0200

    Changed channels order

--- scripts/yt_live.sh
+++ scripts/yt_live.sh
@@ -4,4 +4,4 @@ REGEX_QUERY='(?<=hlsManifestUrl":").*\.m3u8'
 
 BBC_URL=$(curl -s 'https://www.youtube.com/@BBCNewsPersian/live' | grep -oP $REGEX_QUERY)
 ESCAPED_BBC_URL=$(printf '%s\n' "$BBC_URL" | sed -e 's/[\/&]/\\&/g')
-sed -i "7s/.*/$ESCAPED_BBC_URL/" ../ir.m3u
+sed -i "10s/.*/$ESCAPED_BBC_URL/" ../ir.m3u
